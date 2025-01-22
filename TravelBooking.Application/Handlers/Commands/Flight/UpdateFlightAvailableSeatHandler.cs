c340d06f45523208a89d047006df825636c39528
Hashem <Hashem@DESKTOP-64O6ROO>
2025-01-22 21:07:44 +0330

    # feat Update available seats for a flight - feat add update seats on flights controller

---
+++ TravelBooking.Application/Handlers/Commands/Flight/UpdateFlightAvailableSeatHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using TravelBooking.Common.Commands.Flight;
+using TravelBooking.Domain.Interfaces;
+
+namespace TravelBooking.Application.Handlers.Commands.Flight;
+
+public class UpdateFlightAvailableSeatHandler : IRequestHandler<UpdateFlightAvailableSeatCommand, Domain.Entities.Flight>
+{
+    private readonly IFlightRepository _repository;
+
+    public UpdateFlightAvailableSeatHandler(IFlightRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<Domain.Entities.Flight> Handle(UpdateFlightAvailableSeatCommand request, CancellationToken cancellationToken)
+    {
+        var flight = await _repository.GetByFlightNumberAsync(request.FlightNumber);
+
+        if (flight == null)
+            throw new Exception("Flight NotFound");
+
+        flight.AvailableSeats = request.AvailableSeats;
+        await _repository.UpdateAsync(flight);
+        return flight;
+    }
+}
---
+++ TravelBooking.Common/Commands/Flight/UpdateFlightAvailableSeatCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TravelBooking.Common.Commands.Flight;
+
+public class UpdateFlightAvailableSeatCommand: IRequest<Domain.Entities.Flight>
+{
+    public string FlightNumber { get; set; } = string.Empty;
+    public int AvailableSeats { get; set; }
+}
